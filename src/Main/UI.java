382bb1fd66d3a38c851791c4f8ab53d0b4feafac
rojiko1 <rkadekar12@outlook.com>
2022-06-22 16:50:02 -0700

    First commit

---
+++ src/Main/UI.java
@@ -0,0 +1,21 @@
+package Main;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class UI {
+
+    public static void setupWindow() {
+        createWindow();
+    }
+
+    private static void createWindow() {
+        JFrame frame = new JFrame("Sorting Simulator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private static void setText() {
+        // JLabel
+    }
+
+}
---
+++ src/Sorters/BubbleSort.java
@@ -0,0 +1,47 @@
+package Sorters;
+
+import java.util.Arrays;
+
+// comments: sort() has significantly faster execution than recursiveSort(), though both finish in less than a millisecond
+
+public class BubbleSort {
+
+    public int[] sort(int[] array) {
+        for (int i = array.length; i > 0; i--) {
+            for (int j = 0; j < (i - 1); j++) {
+                if (array[j] > array[j + 1]) {
+                    // swap elements
+                    int first = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = first;
+                }
+            }
+        }
+        return array;
+    }
+
+    public int[] recursiveSort(int[] array, int currentDepth) {
+        int[] mod_array;
+        if (currentDepth != array.length) {
+            mod_array = recursiveSort(array, currentDepth + 1);
+        } else {
+            mod_array = array;
+        }
+        for (int i = 0; i < (mod_array.length - 1); i++) {
+            if (mod_array[i] > mod_array[i + 1]) {
+                // swap elements
+                int first = mod_array[i + 1];
+                mod_array[i + 1] = mod_array[i];
+                mod_array[i] = first;
+            }
+        }
+        array[mod_array.length - 1] = mod_array[mod_array.length - 1];
+        if (currentDepth == 1) {
+            return array;
+        }
+        else {
+            return Arrays.copyOf(mod_array, mod_array.length - 1);
+        }
+    }
+
+}
